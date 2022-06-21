4aef19e24237149e4bf74d71db2fcef2e695a334
Sven Mühlberg <sven@muehlberg.net>
2022-06-21 12:20:42 +0200

    change verbose messages

--- erika/erika.py
+++ erika/erika.py
@@ -40,6 +40,7 @@ class Erika:
         self.verbose         = verbose
         self.kbd_name        = 'Erika devel 0.1'
         self.echo            = False
+        # debug
 
     def open(self):
         self.serial.open()
@@ -107,8 +108,8 @@ class Erika:
                         self.serial.write(data)
                 except OSError as e:
                     if e.errno == 11:
-                        if self.verbose:
-                            print('tack',end='',flush=True)
+                        #if self.verbose:
+                        #    print('tack',end='',flush=True)
                         time.sleep(0.5)
                     else:
                         break
@@ -118,6 +119,8 @@ class Erika:
             
     def kbd(self):
         """loop and copy serial->uinput"""
+        # for versoe output
+        keystat=['UP','DOWN']
         # open uinput
         with evdev.UInput(name=self.kbd_name) as  ui:
             time.sleep(1) # wait for evdev.UInput() to settle
@@ -130,35 +133,46 @@ class Erika:
                     if (self.serial.inWaiting() > 0):
                         data = self.serial.read()
                         if data:
+                            # there is serial data
                             kbd_data=data[0]
-                            print(hex(kbd_data),end='')
+                            if self.verbose:
+                                print("erika:",hex(kbd_data),end='')
                             # realisiere Umschaltung durch Code bei einigen Tasten
                             if kbd_data==0xbb:
                                 data = self.serial.read()
                                 kbd_data=data[0]
-                                print("+",hex(kbd_data),end='',sep='')
+                                if self.verbose:
+                                    print("+",hex(kbd_data),end='',sep='')
                                 kbd_data += 0x40
                             if e2i[kbd_data]:
                                 # Jetzt ist die richtige Taste gefunden
-                                print(":",e2i[kbd_data][0],flush=True)
+                                if self.verbose:
+                                    print(" =>",e2i[kbd_data][0],flush=True)
                                 if len(e2i[kbd_data])>1:
+                                    # there are keycodes defined
                                     key_lst=e2i[kbd_data][1]
                                     lenght=len(key_lst)
                                     i=0
                                     while i < lenght:
-                                        print(key_lst[i],key_lst[i+1],sep='=>')
+                                        # iterate over keycodes
+                                        if self.verbose:
+                                            print(e.KEY[key_lst[i]],keystat[key_lst[i+1]],sep='.',end=', ')
                                         ui.write(e.EV_KEY,key_lst[i],key_lst[i+1])
                                         i += 2
                                     ui.syn()
+                                    if self.verbose:
+                                        print(flush=True)
                                 else:
-                                    print("NO KEYS")
+                                    if self.verbose:
+                                        print("NO KEYS",flush=True)
                             else:
-                                # Tastencode nicht bekannt, warne
-                                print(": Unbekannt!",flush=True)
+                                # erika code not defined
+                                print("Error",kbd_data,"unknown!",flush=True)
                     else:
+                        # no serial data, just wait a bit
                         time.sleep(0.2)
-                        if self.verbose:
-                            print('tick',end='',flush=True)
+                        # if self.verbose:
+                        #    print('tick',end='',flush=True)
             except serial.SerialException:
                 self.alive = False
                 raise       # XXX handle instead of re-raise?

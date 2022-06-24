1a37a793ee7660a722c686794a07b06413348ccc
Sven Mühlberg <sven@muehlberg.net>
2022-06-24 08:39:28 +0200

    added module for erika 3004

--- erika/s3004_de.py
+++ erika/s3004_de.py
@@ -1,182 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+
+#  erika.py
+#  
+#  Copyright 2022  <sven@muehlberg.net>
+#  
+#  
 
 import os
 from evdev import ecodes as e
 
-"""
-possible ecodes:
- Event type 1 (EV_KEY)
-    Event code 1 (KEY_ESC)
-    Event code 2 (KEY_1)
-    Event code 3 (KEY_2)
-    Event code 4 (KEY_3)
-    Event code 5 (KEY_4)
-    Event code 6 (KEY_5)
-    Event code 7 (KEY_6)
-    Event code 8 (KEY_7)
-    Event code 9 (KEY_8)
-    Event code 10 (KEY_9)
-    Event code 11 (KEY_0)
-    Event code 12 (KEY_MINUS)
-    Event code 13 (KEY_EQUAL)
-    Event code 14 (KEY_BACKSPACE)
-    Event code 15 (KEY_TAB)
-    Event code 16 (KEY_Q)
-    Event code 17 (KEY_W)
-    Event code 18 (KEY_E)
-    Event code 19 (KEY_R)
-    Event code 20 (KEY_T)
-    Event code 21 (KEY_Y)
-    Event code 22 (KEY_U)
-    Event code 23 (KEY_I)
-    Event code 24 (KEY_O)
-    Event code 25 (KEY_P)
-    Event code 26 (KEY_LEFTBRACE)
-    Event code 27 (KEY_RIGHTBRACE)
-    Event code 28 (KEY_ENTER)
-    Event code 29 (KEY_LEFTCTRL)
-    Event code 30 (KEY_A)
-    Event code 31 (KEY_S)
-    Event code 32 (KEY_D)
-    Event code 33 (KEY_F)
-    Event code 34 (KEY_G)
-    Event code 35 (KEY_H)
-    Event code 36 (KEY_J)
-    Event code 37 (KEY_K)
-    Event code 38 (KEY_L)
-    Event code 39 (KEY_SEMICOLON)
-    Event code 40 (KEY_APOSTROPHE)
-    Event code 41 (KEY_GRAVE)
-    Event code 42 (KEY_LEFTSHIFT)
-    Event code 43 (KEY_BACKSLASH)
-    Event code 44 (KEY_Z)
-    Event code 45 (KEY_X)
-    Event code 46 (KEY_C)
-    Event code 47 (KEY_V)
-    Event code 48 (KEY_B)
-    Event code 49 (KEY_N)
-    Event code 50 (KEY_M)
-    Event code 51 (KEY_COMMA)
-    Event code 52 (KEY_DOT)
-    Event code 53 (KEY_SLASH)
-    Event code 54 (KEY_RIGHTSHIFT)
-    Event code 55 (KEY_KPASTERISK)
-    Event code 56 (KEY_LEFTALT)
-    Event code 57 (KEY_SPACE)
-    Event code 58 (KEY_CAPSLOCK)
-    Event code 59 (KEY_F1)
-    Event code 60 (KEY_F2)
-    Event code 61 (KEY_F3)
-    Event code 62 (KEY_F4)
-    Event code 63 (KEY_F5)
-    Event code 64 (KEY_F6)
-    Event code 65 (KEY_F7)
-    Event code 66 (KEY_F8)
-    Event code 67 (KEY_F9)
-    Event code 68 (KEY_F10)
-    Event code 69 (KEY_NUMLOCK)
-    Event code 70 (KEY_SCROLLLOCK)
-    Event code 71 (KEY_KP7)
-    Event code 72 (KEY_KP8)
-    Event code 73 (KEY_KP9)
-    Event code 74 (KEY_KPMINUS)
-    Event code 75 (KEY_KP4)
-    Event code 76 (KEY_KP5)
-    Event code 77 (KEY_KP6)
-    Event code 78 (KEY_KPPLUS)
-    Event code 79 (KEY_KP1)
-    Event code 80 (KEY_KP2)
-    Event code 81 (KEY_KP3)
-    Event code 82 (KEY_KP0)
-    Event code 83 (KEY_KPDOT)
-    Event code 85 (KEY_ZENKAKUHANKAKU)
-    Event code 86 (KEY_102ND)
-    Event code 87 (KEY_F11)
-    Event code 88 (KEY_F12)
-    Event code 89 (KEY_RO)
-    Event code 90 (KEY_KATAKANA)
-    Event code 91 (KEY_HIRAGANA)
-    Event code 92 (KEY_HENKAN)
-    Event code 93 (KEY_KATAKANAHIRAGANA)
-    Event code 94 (KEY_MUHENKAN)
-    Event code 95 (KEY_KPJPCOMMA)
-    Event code 96 (KEY_KPENTER)
-    Event code 97 (KEY_RIGHTCTRL)
-    Event code 98 (KEY_KPSLASH)
-    Event code 99 (KEY_SYSRQ)
-    Event code 100 (KEY_RIGHTALT)
-    Event code 102 (KEY_HOME)
-    Event code 103 (KEY_UP)
-    Event code 104 (KEY_PAGEUP)
-    Event code 105 (KEY_LEFT)
-    Event code 106 (KEY_RIGHT)
-    Event code 107 (KEY_END)
-    Event code 108 (KEY_DOWN)
-    Event code 109 (KEY_PAGEDOWN)
-    Event code 110 (KEY_INSERT)
-    Event code 111 (KEY_DELETE)
-    Event code 113 (KEY_MUTE)
-    Event code 114 (KEY_VOLUMEDOWN)
-    Event code 115 (KEY_VOLUMEUP)
-    Event code 116 (KEY_POWER)
-    Event code 117 (KEY_KPEQUAL)
-    Event code 119 (KEY_PAUSE)
-    Event code 121 (KEY_KPCOMMA)
-    Event code 122 (KEY_HANGUEL)
-    Event code 123 (KEY_HANJA)
-    Event code 124 (KEY_YEN)
-    Event code 125 (KEY_LEFTMETA)
-    Event code 126 (KEY_RIGHTMETA)
-    Event code 127 (KEY_COMPOSE)
-    Event code 128 (KEY_STOP)
-    Event code 129 (KEY_AGAIN)
-    Event code 130 (KEY_PROPS)
-    Event code 131 (KEY_UNDO)
-    Event code 132 (KEY_FRONT)
-    Event code 133 (KEY_COPY)
-    Event code 134 (KEY_OPEN)
-    Event code 135 (KEY_PASTE)
-    Event code 136 (KEY_FIND)
-    Event code 137 (KEY_CUT)
-    Event code 138 (KEY_HELP)
-    Event code 140 (KEY_CALC)
-    Event code 142 (KEY_SLEEP)
-    Event code 150 (KEY_WWW)
-    Event code 152 (KEY_SCREENLOCK)
-    Event code 158 (KEY_BACK)
-    Event code 159 (KEY_FORWARD)
-    Event code 161 (KEY_EJECTCD)
-    Event code 163 (KEY_NEXTSONG)
-    Event code 164 (KEY_PLAYPAUSE)
-    Event code 165 (KEY_PREVIOUSSONG)
-    Event code 166 (KEY_STOPCD)
-    Event code 173 (KEY_REFRESH)
-    Event code 176 (KEY_EDIT)
-    Event code 177 (KEY_SCROLLUP)
-    Event code 178 (KEY_SCROLLDOWN)
-    Event code 179 (KEY_KPLEFTPAREN)
-    Event code 180 (KEY_KPRIGHTPAREN)
-    Event code 183 (KEY_F13)
-    Event code 184 (KEY_F14)
-    Event code 185 (KEY_F15)
-    Event code 186 (KEY_F16)
-    Event code 187 (KEY_F17)
-    Event code 188 (KEY_F18)
-    Event code 189 (KEY_F19)
-    Event code 190 (KEY_F20)
-    Event code 191 (KEY_F21)
-    Event code 192 (KEY_F22)
-    Event code 193 (KEY_F23)
-    Event code 194 (KEY_F24)
-    Event code 240 (KEY_UNKNOWN)
- """
-
-class s3015:
+class s3004_de:
     def init(self, ui, mui, serial, verbose, echo):
-        self.ui      =ui
-        self.mui     =mui
-        self.serial  =serial
-        self.verbose =verbose
-        self.echo    =echo
+        self.ui      = ui
+        self.mui     = mui
+        self.serial  = serial
+        self.verbose = verbose
+        self.echo    = echo
+        self.step    = 1
 
     # array for erika code to label and list of keycodes 0x00-0xFF) are for native erika codes
     # from 256 (0x100-0x12A) is for 0xbb excaped codes -0xC0, these are done with code key on
@@ -516,14 +357,14 @@ class s3015:
             if self.verbose:
                 print(" =>",self.erika2uinput[kbd_data][0],flush=True)
             if len(self.erika2uinput[kbd_data])>1:
-                # there are keycodes defined
+                # there are ecodes defined
                 if self.echo:
                     return
                 key_lst=self.erika2uinput[kbd_data][1]
                 lenght=len(key_lst)
                 i=0
                 while i < lenght:
-                    # iterate over keycodes
+                    # iterate over ecodes
                     if self.verbose:
                         print(e.KEY[key_lst[i]],keystat[key_lst[i+1]],sep='.',end=', ')
                     self.ui.write(e.EV_KEY,key_lst[i],key_lst[i+1])
@@ -532,7 +373,7 @@ class s3015:
                 if self.verbose:
                     print(flush=True)
             else:
-                # no keycodes defined, maybe a special key
+                # no ecodes defined, maybe a special key
                 if self.verbose:
                     print("NO KEYS - start xkey",flush=True)
                 self.xkey(kbd_data)
@@ -609,14 +450,9 @@ class s3015:
             self.mui.write(e.EV_KEY,e.BTN_LEFT,0)
         elif kbd_data == 0xc7:
             # Mode X - Mouse BTN left - Switch
-            if self.m_btn_left:
-                self.m_btn_left=False
-                self.mui.write(e.EV_KEY,e.BTN_LEFT,0)
-            else:
-                self.m_btn_left=True
-                self.mui.write(e.EV_KEY,e.BTN_LEFT,1)
+            self.mui.write(e.EV_KEY,e.BTN_LEFT,1)
             if self.verbose:
-                print("Mouse BTN left", self.m_btn_left)
+                print("Mouse BTN left On")
         elif kbd_data == 0xcB:
             # Mode C - Mouse BTN right
             if self.verbose:
@@ -625,12 +461,7 @@ class s3015:
             self.mui.write(e.EV_KEY,e.BTN_RIGHT,0)
         elif kbd_data == 0xcF:
             # Mode V - Mouse BTN right - Switch
-            if self.m_btn_right:
-                self.m_btn_right=False
-                self.mui.write(e.EV_KEY,e.BTN_RIGHT,0)
-            else:
-                self.m_btn_right=True
-                self.mui.write(e.EV_KEY,e.BTN_RIGHT,1)
+            self.mui.write(e.EV_KEY,e.BTN_RIGHT,1)
             if self.verbose:
-                print("Mouse BTN right", self.m_btn_right)
+                print("Mouse BTN right On")
         self.mui.syn()

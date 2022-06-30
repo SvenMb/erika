abb5ecb51be8f8383f1dbc7aa4b898874202446d
Sven Mühlberg <sven@muehlberg.net>
2022-06-30 12:22:33 +0200

    service is now configurable

--- src/erika/erika.py
+++ src/erika/erika.py
@@ -107,7 +107,7 @@ class Erika:
 
         # autodetect machine type
         if self.keyboard in ('de'):
-            keyboard='3004_'
+            keyboard='unknown'
             # switch echo print off, this also starts infos from erika about stat and keys
             self.serial.write(b'\x91')
 	        # read max 6 bytes or 0,6s wait
@@ -116,20 +116,22 @@ class Erika:
                     data = self.serial.read()
                     if self.verbose:
                         print("erikastart[",i,"]: ",hex(data[0]),sep='')
-                    if data[0] in (0xf8,0xf9,0xfa,0xfe,0xff):
-                        keyboard='3005_'
+                    if data[0] in (0x84,0x85,0x86,0x87,0x88):
+                        keyboard='3004'
+                    elif data[0] in (0xf8,0xf9,0xfa,0xfe,0xff):
+                        keyboard='3005'
                 else:
                     time.sleep(0.1)
-            self.keyboard=keyboard+self.keyboard
+            self.keyboard=keyboard+'_'+self.keyboard
             # if self.verbose:
-            print("Keyboard     :",self.keyboard)
+            print("Keyboard     :",self.keyboard,flush=True)
 
         if self.keyboard in ('3004_de','3005_de'):
             kbd = getattr(importlib.import_module('erika.s'+self.keyboard),'s'+self.keyboard)()
             # print(self.kbdcl.erika2uinput[0x4f])
         # elif self.keyboard != 'none':
         else:
-            print('unknown keyboard!!!')
+            print('unknown keyboard!!!',flush=True)
             quit()
 
         # set echo on or off

0577bb58d22b9bb773d22fa766fbd2070db80af2
Sven Mühlberg <sven@muehlberg.net>
2022-07-09 15:41:52 +0200

    added stdout as device and made messages compatibel to cups

--- src/erika_set.py
+++ src/erika_set.py
@@ -8,13 +8,15 @@
 #  
 
 import serial
-import sys, getopt
+import os, sys, getopt
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 def main(argv):
 
-    s          =serial.Serial()
-    s.port     ='/dev/erika/erika'
+    s          = serial.Serial()
+    s.port     = '/dev/erika/erika'
     
     verbose    = 0
     cpi        = None
@@ -30,8 +32,8 @@ def main(argv):
         opts, args = getopt.getopt(argv,"hvd:i:s:l:c:f:t:z:",
             ["help","verbose","device=","cpi=","linespacing=","tabstop="])
     except getopt.GetoptError:
-        print('erika.py argument error')
-        print('use erika_set.py -h for help')
+        eprint('erika.py argument error')
+        eprint('use erika_set.py -h for help')
         sys.exit(2)
     for opt, arg in opts:
         if opt in ('-h', '--help'):
@@ -44,6 +46,7 @@ def main(argv):
             print('\t\tcan be added multiple times to be even more verbose')
             print('\t-d, --device <device>\tserial device to use')
             print('\t\tdefault: ', s.port )
+            print('\t\tif \'-\' is given as devicename, stdout is used')
             print('\t-i, --cpi\t\tcharacters per inch, char density')
             print('\t\tuse 10, 12 with normal daisy wheel, 15 possible on\n\t\t3005(and up) with special daisy wheel')
             print('\t-s, --linespacing\t\thalf lines used for one printed line')
@@ -71,77 +74,81 @@ def main(argv):
             if arg in ('10','12','15'):
                 cpi = arg
             else:
-                print('Wrong cpi:', arg)
-                print('only 10, 12 or 15 possible')
+                eprint('ERROR: Wrong cpi:', arg)
+                eprint('ERROR: only 10, 12 or 15 possible')
                 sys.exit(2)
         elif opt in ('-s', '--linespacing'):
             if arg in ('2', '3', '4'):
                 linestep=arg
             else:
-                print('wrong linespacing:',arg)
-                print('only 2, 3 or 4 possible')
+                eprint('ERROR: wrong linespacing:',arg)
+                eprint('ERROR: only 2, 3 or 4 possible')
                 sys.exit(2)
         elif opt in ('-l', '--halflines'):
             if arg.isnumeric() and int(arg) < 256:
                 maxlines = int(arg)
             else:
-                print('wrong halflines ',arg,' used.')
+                eprint('ERROR: wrong halflines ',arg,' used.')
                 sys.exit(2)
         elif opt in ('-c', '--columns'):
             if arg.isnumeric() and int(arg) < 151:
                 maxcolumns = int(arg)
             else:
-                print('wrong columns ',arg,' used.')
+                eprint('ERROR: wrong columns ',arg,' used.')
                 sys.exit(2)
         elif opt in ('-f','--firstcolumn'):
             if arg.isnumeric() and int(arg) > 0:
                 firstcol = int(arg)
             else:
-                print('wrong firstcolumn',arg)
+                eprint('ERROR: wrong firstcolumn',arg)
                 sys.exit(2)
         elif opt in ('-t', '--tabstop'):
             if arg in ('4','8'):
                 tabstop = arg
             else:
-                print('wrong tabstop:',arg)
-                print('only 4 or 8 possible here, use erika daemon setting for other values')
+                eprint('ERROR: wrong tabstop:',arg)
+                eprint('ERROR: only 4 or 8 possible here, use erika daemon setting for other values')
                 sys.exit(2)
         elif opt in ('-z', '--charset'):
             if arg in ('cp858','if6000'):
                 charset = arg
             else:
-                print('wrong extra charset',arg)
+                eprint('ERROR: wrong extra charset',arg)
                 sys.exit(2)
 
 
 
     # info about current options         
     if verbose:
-        print('Erika device:', s.port)
-        print('verbose      :',verbose)
+        eprint('DEBUG: Erika device:', s.port)
+        eprint('DEBUG: verbose      :',verbose)
         if cpi:
-            print('cpi          :', cpi)
+            eprint('DEBUG: cpi          :', cpi)
         if maxcolumns:
-            print('maxcolumns   :', maxcolumns)
+            eprint('DEBUG: maxcolumns   :', maxcolumns)
         if linestep:
-            print('linestep     :', linestep)
+            eprint('DEBUG: linestep     :', linestep)
         if maxlines:
-            print('maxlines     :', maxlines)
+            eprint('DEBUG: maxlines     :', maxlines)
         if firstcol:
-            print('firstcolumn  :', firstcol)
+            eprint('DEBUG: firstcolumn  :', firstcol)
         if tabstop:
-            print('tabstop      :', tabstop)
+            eprint('DEBUG: tabstop      :', tabstop)
         if charset:
-            print('extra charset:', charset)
+            eprint('DEBUG: extra charset:', charset)
 
     # open erika port
-    try:
-        s.open()
-    except serial.SerialException as e:
-        print(e)
-        print('erika daemon not running or wrong device name')
-        sys.exit(2)
-
+    if s.port != '-':
+        try:
+            s.open()
+        except serial.SerialException as e:
+            eprint(e)
+            eprint('CRIT: erika daemon not running or wrong device name')
+            eprint('CRIT: use erika_set.py -h for help')
+            sys.exit(2)
+    # or stdout
+    else:
+        s = os.fdopen(sys.stdout.fileno(), 'wb',closefd=False)
     # cpi
     if cpi:
         if cpi == '15':
@@ -150,7 +157,7 @@ def main(argv):
             s.write(b'\x1bN')
         else: # should be 10 now
             s.write(b'\x1bM')
-        print('cpi set to        :', cpi)
+        eprint('INFO: cpi set to        :', cpi)
 
     # linespacing
     if linestep:
@@ -160,22 +167,22 @@ def main(argv):
             s.write(b'\x1b4')
         else: # should be 4 now
             s.write(b'\x1b5')
-        print('linespacing set to:', linestep)
+        eprint('INFO: linespacing set to:', linestep)
 
     # maxlines
     if maxlines:
         s.write(b'\x1bL' + maxlines.to_bytes(1,'big'))
-        print('maxlines set to:', maxlines)
+        eprint('INFO: maxlines set to:', maxlines)
 
     # maxcolumns
     if maxcolumns:
         s.write(b'\x1bC' + maxcolumns.to_bytes(1,'big'))
-        print('maxcolumns set to:', maxcolumns)
+        eprint('INFO: maxcolumns set to:', maxcolumns)
 
     # firstcolumn
     if firstcol:
         s.write(b'\x1bF' + firstcol.to_bytes(1,'big'))
-        print('firstcolumn set to:', firstcol)
+        eprint('INFO: firstcolumn set to:', firstcol)
     
     # tabstop
     if tabstop:
@@ -183,7 +190,7 @@ def main(argv):
             s.write(b'\x1bT')
         else: # should be 8
             s.write(b'\x1bt')
-        print('tabstop set to:', tabstop)
+        eprint('INFO: tabstop set to:', tabstop)
 
     # charset
     if charset:
@@ -191,7 +198,7 @@ def main(argv):
             s.write(b'\x1bZ')
         else: # should be if6000
             s.write(b'\x1bz')
-        print('charset set to:', charset)
+        eprint('INFO: charset set to:', charset)
     
     # s.write(b'    ')
     

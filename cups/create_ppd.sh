7350b15b1141fc32512840a0b3f197dbadd9be0e
Sven Mühlberg <sven@muehlberg.net>
2022-07-11 23:03:09 +0200

    remove sharing with cups, since it doesn't work well

--- cups/create_ppd.sh
+++ cups/create_ppd.sh
@@ -3,6 +3,7 @@
 sudo cp filter/erika /usr/lib/cups/filter/
 LC_ALL=C ppdc erika.drv
 lpadmin -p Erika -P ppd/erika.ppd -v serial:/dev/erika/erika \
-	-o printer-is-shared=true -L myOffice
+	-L myOffice
+#	-o printer-is-shared=true
 cupsenable Erika
 cupsaccept Erika

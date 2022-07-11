bf1ec6736e12d814e5c37f7458b10f4551250532
Sven Mühlberg <sven@muehlberg.net>
2022-07-11 21:32:15 +0200

    small fix

--- src/install.sh
+++ src/install.sh
@@ -5,5 +5,3 @@ sudo cp -R erika /var/lib/erika/
 sudo cp -R setperm.sh /var/lib/erika/
 sudo cp erika.py erika_set.py /var/lib/erika/
 sudo ln -s /var/lib/erika/erika_set.py /usr/bin/
-sudo systemctl daemon-reload
-sudo systemctl restart erika

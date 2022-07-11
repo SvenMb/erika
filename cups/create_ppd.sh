371206b823822198738e761c90659a38c09292c2
Sven Mühlberg <sven@muehlberg.net>
2022-07-11 12:09:44 +0200

    added autoenable to cups install script

--- cups/create_ppd.sh
+++ cups/create_ppd.sh
@@ -2,4 +2,7 @@
 
 sudo cp filter/erika /usr/lib/cups/filter/
 LC_ALL=C ppdc erika.drv
-lpadmin -p Erika -P ppd/erika.ppd -v serial:/dev/erika/erika
+lpadmin -p Erika -P ppd/erika.ppd -v serial:/dev/erika/erika \
+	-o printer-is-shared=true -L myOffice
+cupsenable Erika
+cupsaccept Erika
